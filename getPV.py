85f16200b75e04ba1a06f87816df1e6f126b294b
carneirofc <claudiofcarneiro@hotmail.com>
2020-03-03 20:10:52 +0000

    src: Removed invalid PVs

--- getPV.py
+++ getPV.py
@@ -2,14 +2,19 @@
 import requests
 import json
 
-url = 'https://10.0.38.42/retrieval/bpl/getMatchingPVs?pv=SI*CCG*Pressure-Mon&limit=400'
+# url = 'https://10.0.38.42/retrieval/bpl/getMatchingPVs?pv=SI*CCG*Pressure-Mon&limit=400'
+# "Being archived"
+# connectionState "true"
+url = 'https://10.0.38.42/mgmt/bpl/getPVStatus?pv=SI*CCG*Pressure-Mon&limit=4000'
 fe = []
 si = []
 for data in requests.get(url, verify=False).json():
-    if 'FE' in data:
-        fe.append(data)
-    else:
-        si.append(data)
+    if data['status'] == "Being archived":
+        if data['connectionState'] == 'true':
+            if 'FE' in data['pvName']:
+                fe.append(data['pvName'])
+            else:
+                si.append(data['pvName'])
 
 with open('src/static/SI-CCG.json', 'w+') as _f:
     json.dump(si, _f)
