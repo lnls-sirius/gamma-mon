6311313994aafb487228d3ffc8fd64a1d7bc4a02
carneirofc <claudiofcarneiro@hotmail.com>
2020-03-26 10:43:52 -0300

    Initial tests of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,162 +1,18 @@
 import React from 'react';
-import PressureBar from './components/PressureBar';
-
-import { Button, ButtonGroup } from '@material-ui/core';
-
-import bo from './static/BO-CCG.json';
-import si from './static/SI-CCG.json';
-import fe from './static/FE-CCG.json';
-import tb from './static/TB-CCG.json';
-import ts from './static/TS-CCG.json';
-
-import { SI, BO } from './utils/consts';
+import Router from './Router';
 
 import './App.css';
-
-const STATE = {
-  INITIAL: 0,
-  BO: 1,
-  SI: 2,
-  TB: 3,
-  TS: 4,
-  ALL: 5,
-  TB_TS: 6,
-  BO_TB_TS: 7,
-  FE: 8,
-  SI_FE: 9,
-  BO_TB_TS_FE: 10
-}
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { content: STATE.INITIAL, tooltipVisible: false, tooltipX: '', tooltipY: '' };
   }
-
-  customTooltipCallback = (tooltipModel) => {
-
-    if (tooltipModel.opacity === 0) {
-      this.setState({ tooltipVisible: false, tooltipX: '', tooltipY: '' });
-      return;
-    }
-
-    // set values
-    const x = tooltipModel.dataPoints[0].xLabel;
-    const y = tooltipModel.dataPoints[0].yLabel;
-
-    this.setState({ tooltipVisible: true, tooltipX: x, tooltipY: y });
-  }
-
-  renderNav = () => {
-    if (this.state.content !== STATE.INITIAL) {
-      return <div className='Menu'>
-        <Button variant="contained" color="default" onClick={() => this.setState({ content: STATE.INITIAL })}>Back</Button>
-      </div>
-    } else {
-      return <div className='Menu'>
-        <div className='MainTitle'>Sirius - Pressure Readings</div>
-        <div style={{ 'margin-bottom': '15px' }} className='SubTitle'>Cold Cathode Gauge</div>
-        <ButtonGroup orientation="vertical" color="primary"
-        >
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.BO })}>BO</Button><br />
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.SI })}>SI</Button><br />
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.TB })}>TB</Button><br />
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.TS })}>TS</Button><br />
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.FE })}>FE</Button><br />
-          <div><br></br></div>
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.TB_TS })}>TB & TS</Button><br />
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.BO_TB_TS })}>BO, TB & TS</Button><br />
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.BO_TB_TS_FE })}>BO, TB, TS & FE</Button><br />
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.SI_FE })}>SI & FE</Button><br />
-          <Button variant="contained" color="primary" onClick={() => this.setState({ content: STATE.ALL })}>ALL</Button><br />
-        </ButtonGroup>
-      </div>
-
-    }
-  }
-
-  renderCustomTooltip = () => {
-    return <table align='center'>
-      <tbody>
-        {this.state.tooltipVisible ? (
-          <tr>
-            <td>{this.state.tooltipX}</td>
-            <td>{this.state.tooltipY}</td>
-          </tr>
-        ) : (<tr>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>)}
-      </tbody>
-    </table>
-  }
-
-  backHandler = () => this.setState({ content: STATE.INITIAL })
-
-  renderGraph = () => {
-    switch (this.state.content) {
-      case STATE.BO:
-        return <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={bo} title='BO - Pressure' {...BO} />
-      case STATE.TB:
-        return <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={tb} title='TB - Pressure'  {...BO} />
-      case STATE.TS:
-        return <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={ts} title='TS - Pressure'  {...BO} />
-      case STATE.SI:
-        return <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={si} title='SI - Pressure'  {...SI} />
-      case STATE.FE:
-        return <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={fe} title='FE - Pressure'  {...SI} />
-
-
-      case STATE.TB_TS:
-        return <div style={{ 'display': 'flex', 'flex-direction': 'row', 'flex-wrap': 'wrap' }}>
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={tb} title='TB - Pressure'  {...BO} />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={ts} title='TS - Pressure'  {...BO} />
-        </div>
-      case STATE.BO_TB_TS:
-        return <div style={{ 'display': 'flex', 'flex-direction': 'row', 'flex-wrap': 'wrap' }}>
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={bo} title='BO - Pressure' {...BO} />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={tb} title='TB - Pressure'  {...BO} />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={ts} title='TS - Pressure'  {...BO} />
-        </div>
-      case STATE.BO_TB_TS_FE:
-        return <div style={{ 'display': 'flex', 'flex-direction': 'row', 'flex-wrap': 'wrap' }}>
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={bo} title='BO - Pressure'  {...BO} />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={tb} title='TB - Pressure'  {...BO} />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={ts} title='TS - Pressure'  {...BO} />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={fe} title='FE - Pressure'  {...SI} />
-        </div>
-
-      case STATE.SI_FE:
-        return <div>
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={si} title='SI - Pressure' {...SI} />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={fe} title='FE - Pressure' {...SI} />
-        </div>
-
-
-      case STATE.ALL:
-        return <div style={{ 'display': 'flex', 'flex-direction': 'row', 'flex-wrap': 'wrap' }}>
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={bo} title='BO - Pressure' />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={tb} title='TB - Pressure' />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={ts} title='TS - Pressure' />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={si} title='SI - Pressure' />
-          <PressureBar customTooltipCallback={this.customTooltipCallback} pvs={fe} title='FE - Pressure' />
-        </div>
-
-      default:
-        if (this.state.tooltipVisible) { this.setState({ tooltipVisible: false }); }
-        return <div></div>
-    }
-  }
-
   render() {
     return (
       <div className="App">
-        {this.renderGraph()}
-        {this.renderCustomTooltip()}
-        {this.renderNav()}
+        <Router />
       </div>
     );
   }
-
 }
 export default App;
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,35 +1,9 @@
 const SI = { hihi: 5e-8, high: 1e-8 };
+
+/** URL used for routing */
+const BASE_URL = '/OPI/VACS';
+const CCG_URL = BASE_URL + '/CCG';
+export { BASE_URL, CCG_URL};
+
 const BO = { hihi: 5e-7, high: 1e-7 };
 export { SI, BO };
-
-// class ProcessVariables {
-//     constructor(onComplete, onFailure ){
-//         if(!onComplete){
-//             this.onComplete = (res)=>{console.log(res)};
-//         }
-//         if(!onFailure){
-//             this.onFailure = (res)=>{console.log(res)};
-//         }
-//         this.loaded = false;
-//         this.pvs = [];
-//     }
-// }
-
-
-// var state = false;
-// var booster = [];
-
-// const URL = "https://10.0.38.42/mgmt/bpl/getPVStatus?pv=BO*CCG*Pressure-Mon&limit=200";
-
-// fetch(URL)
-//     .then(data=>data.json())
-//     .then(res=>{
-//         console.log(res);
-//         res.forEach(element => {
-//             booster.push(element.pvName);
-//         });
-//         state=true;
-//     })
-//     .catch(exception=>console.log(exception));
-
-// export { booster };
