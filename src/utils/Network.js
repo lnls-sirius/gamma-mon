bb83b0ae5e43109839b28edea942f1064fe70f17
rafael.lyra <rafael.lyra@lnls.br>
2026-09-09 08:45:50 -0300

    Use machine DNS instead of IP
    Using the IP with HTTPS is causing the socket connection to crash.

--- src/utils/Network.js
+++ src/utils/Network.js
@@ -4,7 +4,7 @@ class Network {
             Network.instance = this;
         }
         this.secure = window.location.protocol === 'https:';
-        this.epics2webHost = '10.0.38.42';
+        this.epics2webHost = 'ais-eng-srv-ta.cnpem.br';
         this.epics2webLocation = '/epics2web';
         this.epics2webGet = this.secure ? 'https://' : 'http://' + this.epics2webHost + this.epics2webLocation + '/caget';
         this.epics2webWs =
